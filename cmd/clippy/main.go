8e8ec0f80782d20601401e60866d3a9a32ca9fd1
Alan Jose <alanjose10@gmail.com>
2026-04-19 23:40:10 +0100

    adding static assets

--- cmd/clippy/main.go
+++ cmd/clippy/main.go
@@ -1,21 +1,3 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	"log"
-
-	"clippy/internal/db"
-)
-
-func main() {
-	ctx := context.Background()
-
-	database, err := db.Open(ctx, "./test.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer database.Close()
-
-	fmt.Println("db opened successfully")
-}
+func main() {}
